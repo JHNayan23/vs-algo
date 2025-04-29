efb488e50917bd7ccd092694da0e52e3f251d157
JHNayan23 <jhnayandx704@gmail.com>
2025-04-29 23:44:19 +0600

    first commit

---
+++ Sorting_Visualizer/scripts/visualizations.js
@@ -0,0 +1,65 @@
+/*
+    *****************
+    DONE BY:-   Jabir Nayan
+    
+    *****************
+*/
+
+//We only have to change background-color and height of the sorting element.
+
+var speed=1000;
+
+inp_aspeed.addEventListener("input",vis_speed);
+
+function vis_speed() //sets the sorting visualization speed based on the value from an input element
+{
+    var array_speed=inp_aspeed.value;
+    switch(parseInt(array_speed))
+    {
+        case 1: speed=1;
+                break;
+        case 2: speed=10;
+                break;
+        case 3: speed=100;
+                break;
+        case 4: speed=1000;
+                break;
+        case 5: speed=10000;
+                break;
+    }
+    
+    delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+}
+
+var delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+var c_delay=0;//This is updated ov every div change so that visualization is visible.
+
+function div_update(cont,height,color) //function use to reflect changes at each step of the algorithm
+{
+    window.setTimeout(function(){
+        cont.style=" margin:0% " + margin_size + "%; width:" + (100/array_size-(2*margin_size)) + "%; height:" + height + "%; background-color:" + color + ";";
+    },c_delay+=delay_time);
+}
+
+function enable_buttons() //function to re-enables all buttons and input fields after the sorting process is complete.
+{
+    window.setTimeout(function(){
+        for(var i=0;i<butts_algos.length;i++)
+        {
+            butts_algos[i].classList=[];
+            butts_algos[i].classList.add("butt_unselected");
+
+            butts_algos[i].disabled=false;
+            inp_as.disabled=false;
+            inp_gen.disabled=false;
+            inp_aspeed.disabled=false;
+        }
+    },c_delay+=delay_time);
+}
+
+/*
+    *****************
+    DONE BY:-   Jabir Nayan
+    
+    *****************
+*/
